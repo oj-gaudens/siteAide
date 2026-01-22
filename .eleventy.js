585a3d19da09b9f2ab337a0219869a5faec3ffe8
oumar-julien gaudens <oumar-julien.gaudens@i-carre.net>
2026-01-22 16:29:35 +0100

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,11 @@
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
-  const isProd = process.env.ELEVENTY_ENV === "production";
+
   return {
     dir: {
       input: "src",
       output: "_site",
       includes: "_includes"
-    },
-    pathPrefix: isProd ? "/Site-Aide/" : "/"
+    }
   };
 };
